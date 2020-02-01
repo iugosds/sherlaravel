18fc3d41d0494263b9f5d9c4bcc049c01b27dba4
Gonzalo Medeiros <gmedeiros@iugo.com.uy>
2020-02-01 14:02:40 -0300

    Added an enabled/disabled flag in configuration

--- config/sherlaravel.php
+++ config/sherlaravel.php
@@ -4,4 +4,5 @@ return [
     'apiUrl' => 'https://in.sherlockscore.com/v1',
     'waitTimeout' => 5,
     'apiKey' => env('SHERLOCK_SCORE_API_KEY', null),
+    'enabled' => env('SHERLOCK_SCORE_ENABLED', true),
 ];
